6ad60e538f0593e02ec67442ef2730a2e96329c4
jnf <hi@jnf.dev>
2021-12-12 13:09:20 -0800

    d4 requires a small change to the stringReader -- now we can pass in the pattern for splitting input lines

--- shared.js
+++ shared.js
@@ -1,8 +1,8 @@
 import fs from 'fs'
 import readline from 'readline'
 
-const stringReader = path =>
-  fs.readFileSync(path, 'utf8').trim().split("\n")
+const stringReader = (path, splitPattern='\n') =>
+  fs.readFileSync(path, 'utf8').trim().split(splitPattern)
 
 const numberReader = path =>
   stringReader(path).map(Number)
